dda311a2aac9511cdb62b2abe619f14a0683b0bd
dvo4253 <8908406+dvo4253@users.noreply.github.com>
2019-12-10 09:37:31 -0600

    Added dry-runs

--- git.sh
+++ git.sh
@@ -1,2 +1,4 @@
-alias gpob='git push origin $(git rev-parse --abbrev-ref HEAD)'
 alias gpo='git push origin'
+alias gpodr='git push --dry-run origin'
+alias gpob='git push origin $(git rev-parse --abbrev-ref HEAD)'
+alias gpobdr='git push --dry-run origin $(git rev-parse --abbrev-ref HEAD)'
